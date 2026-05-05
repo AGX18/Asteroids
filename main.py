4b89fbbe0f5c9cbd9031135ac8f5b489ea89c437
AGX <agxnh2020@gmail.com>
2026-05-05 11:55:45 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+import constants
+
+def main():
+    # pygame.init()
+
+    # screen = pygame.display.set_mode((800, 600))
+
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
